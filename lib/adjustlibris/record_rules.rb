ec76961f66d6f0a7e4c25a3eeab6c9fd543a545c
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2016-10-26 12:27:41 +0200

    One rule with a test.

--- lib/adjustlibris/record_rules.rb
+++ lib/adjustlibris/record_rules.rb
@@ -2,6 +2,21 @@ class AdjustLibris
   class RecordRules
     # Apply in turn each and every rule, and return a record in the end.
     def self.apply(record)
+      record = rule_041(record)
+      record
+    end
+
+    # If 041 does not exist, copy 008/35-37 to 041$a,
+    #   unless 008/35-37 is ["und", "xxx", "mul"]
+    def self.rule_041(record)
+      f008 = record["008"]
+      if !f008
+        raise StandardError, "ControlField 008 missing"
+      end
+      lang = f008.value[35..37]
+      if !["und", "xxx", "mul"].include?(lang)
+        record.append(MARC::DataField.new('041', '', '', ['a', lang]))
+      end
       record
     end
   end
--- spec/adjustlibris/record_rules_spec.rb
+++ spec/adjustlibris/record_rules_spec.rb
@@ -1,13 +1,17 @@
 describe "AdjustLibris::RecordRules" do
-  before :each do
-    marc21_file = "spec/data/valid-marc21.mrc"
-    @record1 = MARC::Reader.new(marc21_file).first
-  end
   
   context "Rules" do
-    # Dummy test since there are no rules
-    it "should apply rule 1" do
+    context "rule_041" do
+      before :each do
+        marc21_file = "spec/data/without-041.mrc"
+        @record = MARC::Reader.new(marc21_file).first
+      end
       
+      it "should apply rule" do
+        new_record = AdjustLibris::RecordRules.rule_041(@record)
+        expect(new_record["041"]).to be_kind_of(MARC::DataField)
+        expect(new_record["041"]["a"]).to eq("eng")
+      end
     end
   end
 end
