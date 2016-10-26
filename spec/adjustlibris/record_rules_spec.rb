c369c8c97433b7800954e6b43c25d3f55c2a66ef
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2016-10-26 12:57:57 +0200

    Extend tests to cover falsey cases too

--- spec/adjustlibris/record_rules_spec.rb
+++ spec/adjustlibris/record_rules_spec.rb
@@ -3,14 +3,26 @@ describe "AdjustLibris::RecordRules" do
   context "Rules" do
     context "rule_041" do
       before :each do
-        marc21_file = "spec/data/without-041.mrc"
-        @record = MARC::Reader.new(marc21_file).first
+        @record_without_041 = MARC::Reader.new("spec/data/rule_041-without_041.mrc").first
+        @record_with_041 = MARC::Reader.new("spec/data/rule_041-with_041.mrc").first
+        @record_wrong_lang = MARC::Reader.new("spec/data/rule_041-wrong_lang.mrc").first
       end
       
-      it "should apply rule" do
-        new_record = AdjustLibris::RecordRules.rule_041(@record)
+      it "should add 041$a when it does not exist" do
+        new_record = AdjustLibris::RecordRules.rule_041(@record_without_041)
         expect(new_record["041"]).to be_kind_of(MARC::DataField)
         expect(new_record["041"]["a"]).to eq("eng")
+      end
+
+      it "should not touch 041$a when it already exists" do
+        new_record = AdjustLibris::RecordRules.rule_041(@record_with_041)
+        expect(new_record["041"]).to be_kind_of(MARC::DataField)
+        expect(new_record["041"]["a"]).to eq(@record_with_041["041"]["a"])
+      end
+
+      it "should not create 041$a if 008/35-37 is und, xxx or mul" do
+        new_record = AdjustLibris::RecordRules.rule_041(@record_wrong_lang)
+        expect(new_record["041"]).to_not be_kind_of(MARC::DataField)
       end
     end
   end
